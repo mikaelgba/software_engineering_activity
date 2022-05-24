833d8df71c49ea561a9b73ac1cf54305bb3de791
michael.oliveira <michael.oliveira@virtus.ufcg.edu.br>
2022-05-24 16:08:46 -0300

    update

--- src/ufpb/br/Michael.java
+++ src/ufpb/br/Michael.java
@@ -3,15 +3,24 @@ package ufpb.br;
 public class Michael {
 
     private static String name = "michael Uewerton Targino de Oliveira";
-    private int numberOne = 1;
-    private int numberTwo = 2;
-    private int numberthree;
+    private static int numberOne = 1;
+    private static int numberTwo = 2;
+    private static int numberthree;
 
-    public int SumNuberMichael(){
-        return this.numberthree = this.numberOne + this.numberTwo;
+    public static int SumNuberMichael(){
+        return numberthree = numberOne + numberTwo;
+    }
+
+    public static String printName(){
+        return getName();
+    }
+
+    public static String getName() {
+        return name;
     }
 
     public static void main(String[] args){
-        System.out.println(name);
+        System.out.println(printName());
+        System.out.println(SumNuberMichael());
     }
 }
