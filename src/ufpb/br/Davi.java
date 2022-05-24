5726f0679b57b6196e183dad1538aa68fc9f6130
DaviCorreia <davicorreia21@gmail.com>
2022-05-24 15:25:35 -0300

    adicionando classe Davi

---
+++ src/ufpb/br/Davi.java
@@ -0,0 +1,12 @@
+package ufpb.br;
+
+public class Davi {
+
+    private String name = "Davi Correia de oliveira";
+    private int num1 =1;
+    private int num2 =2;
+    private int num3 ;
+    public int somaNumerosDavi(int num1, int num2){
+        return this.num3= num1+num2;
+    }
+}
