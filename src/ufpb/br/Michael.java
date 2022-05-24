22b3c4c810998789afc54592fd6eb637e9061444
michael.oliveira <michael.oliveira@virtus.ufcg.edu.br>
2022-05-24 15:24:47 -0300

    up

--- src/ufpb/br/Michael.java
+++ src/ufpb/br/Michael.java
@@ -1,2 +1,17 @@
-package ufpb.br;public class Michael {
+package ufpb.br;
+
+public class Michael {
+
+    private static String name = "michael Uewerton Targino de Oliveira";
+    private int numberOne = 1;
+    private int numberTwo = 2;
+    private int numberthree;
+
+    public int SumNuberMichael(){
+        return this.numberthree = this.numberOne + this.numberTwo;
+    }
+
+    public static void main(String[] args){
+        System.out.println(name);
+    }
 }
